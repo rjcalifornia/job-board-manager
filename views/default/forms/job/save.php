731d37a849c69e7a886f98277c1cd9bbebdac60c
rjcalifornia <rodolfohernan20@gmail.com>
2024-10-28 15:55:12 -0600

    Changes to be committed: 	modified:   resources/forms/save.html.twig 	modified:   views/default/forms/job/save.php

--- views/default/forms/job/save.php
+++ views/default/forms/job/save.php
@@ -18,18 +18,50 @@ $container_guid = elgg_extract('container_guid', $vars);
  
 $title = elgg_view_field([
     '#label' => elgg_echo('title'),
-		'#type' => 'text',
-		'required' => true,
-		'name' => 'title',
-		'value' => elgg_extract('title', $vars),
+    '#type' => 'text',
+    'required' => true,
+    'name' => 'title',
+    'value' => elgg_extract('title', $vars),
 ]);
 
 $overview = elgg_view_field([
     '#label' => elgg_echo('job:overview'),
-		'#type' => 'longtext',
-		'required' => true,
-		'name' => 'overview',
-		'value' => elgg_extract('overview', $vars),
+    '#type' => 'longtext',
+    'required' => true,
+    'name' => 'overview',
+    'value' => elgg_extract('overview', $vars),
+]);
+
+$qualifications = elgg_view_field([
+    '#label' => elgg_echo('job:qualifications'),
+    '#type' => 'longtext',
+    'required' => true,
+    'name' => 'qualifications',
+    'value' => elgg_extract('qualifications', $vars),
+]);
+
+$responsabilities = elgg_view_field([
+    '#label' => elgg_echo('job:responsabilities'),
+    '#type' => 'longtext',
+    'required' => false,
+    'name' => 'responsabilities',
+    'value' => elgg_extract('responsabilities', $vars),
+]);
+
+$salary = elgg_view_field([
+    '#label' => elgg_echo('job:salary'),
+    '#type' => 'text',
+    'required' => false,
+    'name' => 'salary',
+    'value' => elgg_extract('salary', $vars),
+]);
+
+$tags = elgg_view_field([
+    '#label' => elgg_echo('tags'),
+    '#type' => 'tags',
+    'name' => 'tags',
+    'id' => 'blog_tags',
+    'value' => elgg_extract('tags', $vars),
 ]);
 
 $accesId = elgg_view_field([
@@ -65,6 +97,9 @@ $footer = elgg_view_field([
  
 $data['title_field'] = new \Twig\Markup($title, 'UTF-8');
 $data['overview_field'] = new \Twig\Markup($overview, 'UTF-8');
+$data['qualifications_field'] = new \Twig\Markup($qualifications, 'UTF-8');
+$data['responsabilities_field'] = new \Twig\Markup($responsabilities, 'UTF-8');
+$data['tags_field'] = new \Twig\Markup($tags, 'UTF-8');
 $data['access_id'] = new \Twig\Markup($accesId, 'UTF-8');
 
 
