2162c7939660d3452522d903541feb2568227629
rjcalifornia <rodolfohernan20@gmail.com>
2024-11-12 10:56:40 -0600

    Changes to be committed: 	modified:   actions/job/apply.php 	modified:   resources/forms/apply.html.twig 	modified:   src/index.html

--- actions/job/apply.php
+++ actions/job/apply.php
@@ -1,1 +1,32 @@
 <?php
+
+use JobUtils;
+
+$jobUtils = new JobUtils;
+
+$guid = get_input('__elgg_guid');
+$firstName = get_input('first_name');
+$lastName = get_input('last_name');
+$email = get_input('email');
+$phone = get_input('phone');
+$coverLetter = get_input('cover_letter');
+
+
+$entity = get_entity($guid);
+
+$application = new \ElggJobApplication;
+
+$application->first_name = $firstName;
+$application->last_name = $lastName;
+$application->email = $email;
+$application->phone = $phone;
+$application->cover_letter = $coverLetter;
+$application->container_guid = $guid;
+//$application->resume = $file;
+$application->application_identifier = $jobUtils->generateIdentifier();
+
+if (!$application->save()) {
+	return elgg_error_response(elgg_echo('job:application:failed'));
+}
+
+return elgg_ok_response('', elgg_echo('job:save:success'), $entity->getURL());
