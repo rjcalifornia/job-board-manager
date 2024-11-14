781b0ef8254c0944605597a48b190f3334dbd831
rjcalifornia <rodolfohernan20@gmail.com>
2024-11-14 11:49:23 -0600

    Changes to be committed: 	modified:   resources/job/elements/sidebar.html.twig 	modified:   views/default/job/sidebar.php

--- views/default/job/sidebar.php
+++ views/default/job/sidebar.php
@@ -4,8 +4,29 @@
  */
 $twig = jobs_twig();
 $view_mode = elgg_extract('single', $vars);
+
+$defaults = [
+	'type' => 'object',
+	'subtype' => 'job',
+	'full_view' => false, 
+	'distinct' => false,
+    'limit' => 2,
+];
+
+$options = (array) elgg_extract('options', $vars, []);
+$options = array_merge($defaults, $options);
+
+$entities= elgg_get_entities($defaults);
+
+foreach ($entities as $entity) {
+	$entity->url = $entity->getUrl();
+}
+ 
+
 $data = [
-    'view_mode' => $view_mode
+    'site_url' => elgg_get_site_url(),
+    'entities' => $entities,
+    'view_mode' => $view_mode,
 ];
 
 
